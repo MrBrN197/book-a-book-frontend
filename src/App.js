d4237f267d28bba38738dc1a8ca029250dcbda82
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-03-01 21:19:42 +0100

    Add route that leads to reservations page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ReservationForm from './components/reservations/reservationForm';
+import ReservationForm from './components/reservations/ReservationForm';
 import './index.css';
 import Navbar from './components/navbar/Navbar';
 import Error404 from './components/errors/Error404';
 import AuthRoute from './components/auth/AuthRoute';
 import LoginPage from './components/auth/LoginPage';
+import ReservationsPage from './components/reservations/ReservationsPage';
 
 const MainPage = () => <div>🐋 Main Page</div>;
 
@@ -17,10 +18,10 @@ const App = () => (
         <Route path="books" element={<p>* books *</p>} />
         <Route path="books/new" element={<p>* add book *</p>} />
         <Route path="books/remove" element={<p>* delete book *</p>} />
-        <Route path="reservations" element={<p>* reservations *</p>} />
+        <Route path="reservations" element={<AuthRoute Component={<ReservationsPage />} />} />
         <Route path="*" element={<Error404 />} />
       </Route>
-      <Route path="reservations/new" element={<ReservationForm />} />
+      <Route path="reservations/new" element={<AuthRoute Component={<ReservationForm />} />} />
     </Routes>
   </Router>
 );
