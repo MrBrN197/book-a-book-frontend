d6e68197a31f2268d0c9fe347ca255a7b0d9dd1c
brianorora <brianorora@gmail.com>
2022-03-02 19:55:10 +0300

    Add `Navbar` component

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,31 +1,44 @@
 import { Link, Outlet } from 'react-router-dom';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { IoMdClose } from 'react-icons/io';
+import { useState } from 'react';
 import styles from './Navbar.module.scss';
+import '../index.scss';
 
-const Navbar = () => (
-  <div className={styles.App}>
-    <section className={styles.navbar}>
-      <Link to="/"><h1>Book A Book</h1></Link>
-      <ul className={styles.links}>
-        <li><Link to="/books">BOOKS</Link></li>
-        <li><Link to="/reservations">MY RESERVATIONS</Link></li>
-        <li><Link to="/books/new">ADD BOOK</Link></li>
-        <li><Link to="/books/remove">DELETE BOOK</Link></li>
-        <li><Link to="/reservations/new">RESERVE BOOK</Link></li>
-      </ul>
-      <section>
-        <ul className={styles.social}>
-          <i className="" />
-          <i className="" />
-          <i className="" />
-          <i className="" />
+const Navbar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+  const toggleNavbar = () => {
+    setNavbarOpen((s) => !s);
+  };
+
+  return (
+    <div className={styles.App}>
+      <section className={styles.navbar}>
+        <GiHamburgerMenu className={`${styles.navicon} mobile-only`} role="button" onClick={toggleNavbar} />
+        <Link to="/"><h1>Book A Book</h1></Link>
+        <ul className={`${styles.links} ${navbarOpen ? styles.open : ''}`}>
+          <IoMdClose onClick={toggleNavbar} className={`${styles.navicon} mobile-only`} />
+          <li><Link to="/books">BOOKS</Link></li>
+          <li><Link to="/reservations">MY RESERVATIONS</Link></li>
+          <li><Link to="/books/new">ADD BOOK</Link></li>
+          <li><Link to="/books/remove">DELETE BOOK</Link></li>
+          <li><Link to="/reservations/new">RESERVE BOOK</Link></li>
         </ul>
-        <span className={styles.copyright}>
-          &copy; 2015 Copyright
-        </span>
+        <section className="desktop-only">
+          <ul className={styles.social}>
+            <i className="" />
+            <i className="" />
+            <i className="" />
+            <i className="" />
+          </ul>
+          <span className={styles.copyright}>
+            &copy; 2015 Copyright
+          </span>
+        </section>
       </section>
-    </section>
-    <Outlet />
-  </div>
-);
+      <Outlet />
+    </div>
+  );
+};
 
 export default Navbar;
