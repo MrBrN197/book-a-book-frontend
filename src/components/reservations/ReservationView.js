41dfa2bec3b10f9c928ad100cbc99230fe8a4916
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-03-03 12:29:10 +0100

    Design the basic layout for an individual reservation

--- src/components/reservations/ReservationView.js
+++ src/components/reservations/ReservationView.js
@@ -1,27 +1,32 @@
+import './reservation.css';
 import PropTypes from 'prop-types';
 
 const ReservationView = ({ reservation, book }) => {
   const date = new Date(reservation.reservation_date);
   const formattedDate = new Intl.DateTimeFormat('en-us', { dateStyle: 'full', timeStyle: 'short' }).format(date);
   return (
-    <div>
-      <div>
+    <article className="reservationview-container d-flex">
+      <div className="img-placeholder">
         <img src={book.image} alt={`Book cover of ${book.title}`} />
       </div>
-      <p>
-        Reservation Date:
-        {' '}
-        {formattedDate}
-      </p>
-      <p>
-        {book.title}
-        {' '}
-        by
-        {' '}
-        {book.author}
-      </p>
-      <p>{reservation.city}</p>
-    </div>
+      <div className="details-div">
+        <div className="book-details d-flex">
+          <h1>{book.title}</h1>
+          <span className="by">by</span>
+          <span className="author">{book.author}</span>
+          <span className="price">
+            {' '}
+            $
+            {book.price}
+          </span>
+        </div>
+        <p className="city">{reservation.city}</p>
+        <div className="reservation-details d-flex">
+          <span className="reservation-date">Rental Date</span>
+          <span className="word-date">{formattedDate}</span>
+        </div>
+      </div>
+    </article>
   );
 };
 
@@ -30,6 +35,7 @@ ReservationView.propTypes = {
     title: PropTypes.string,
     image: PropTypes.string,
     author: PropTypes.string,
+    price: PropTypes.string,
   }).isRequired,
   reservation: PropTypes.shape({
     reservation_date: PropTypes.string,
